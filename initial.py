e46a48835431c4d8dcd06f45f12a03cfb66aec65
Boateng24 <56285760+Boateng24@users.noreply.github.com>
2021-10-13 06:04:09 -0700

    first commit

---
+++ initial.py
@@ -0,0 +1,24 @@
+# Tuffour Boateng
+# Fun Fact: my science life contradicts my love life
+name1 = """ 
+           TTT
+          T   T
+          T
+           TTT
+              T
+          T   T
+           TTT
+      """
+
+name2  = """
+
+            BBB
+           B   B
+           B
+            BBB
+                B
+           B    B
+            BBB
+"""        
+print(name1)
+print(name2)
